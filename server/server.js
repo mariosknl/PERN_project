845008d152c176a91d81a96bb6e79d9f9b9d051e
mariosknl <mariosknl@outlook.com>
2020-12-31 15:51:49 +0200

    api for creating new restaurants created

--- server/server.js
+++ server/server.js
@@ -42,14 +42,22 @@ app.get("/api/v1/restaurant/:id", async (req, res) => {
 });
 
 // Create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      restaurant: "mcdonalds",
-    },
-  });
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const results = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) values ($1, $2, $3) returning *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    console.log(results);
+    res.status(201).json({
+      status: "success",
+      data: {
+        restaurant: results.rows[0],
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 // Update Restaurants
