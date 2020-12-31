5dab1abbe9d996061229b3cd73bfc370046798af
mariosknl <mariosknl@outlook.com>
2020-12-31 16:48:34 +0200

    establishing routes for the frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/restaurants/:id/update" component={UpdatePage} />
+          <Route path="/restaurants/:id" component={RestaurantDetailPage} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RestaurantDetailPage = () => {
+  return <div>Details</div>;
+};
+
+export default RestaurantDetailPage;
